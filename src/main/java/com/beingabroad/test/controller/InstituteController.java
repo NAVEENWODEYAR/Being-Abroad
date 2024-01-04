f60d6876bbaeffebcad1a059026919fc1111df52
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-04 23:38:21 +0530

    swagger added

--- src/main/java/com/beingabroad/test/controller/InstituteController.java
+++ src/main/java/com/beingabroad/test/controller/InstituteController.java
@@ -1,7 +1,18 @@
 package com.beingabroad.test.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InstituteController {
+
+    // testEndPoint,
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.CONTINUE)
+    public String testEndPoint()
+    {
+        return "Welcome!";
+    }
 }
