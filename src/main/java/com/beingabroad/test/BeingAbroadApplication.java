2b6b4f97fa05f83b95a21fc5343fecffdf31211a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-04 23:05:59 +0530

    initial commit

---
+++ src/main/java/com/beingabroad/test/BeingAbroadApplication.java
@@ -0,0 +1,14 @@
+package com.beingabroad.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeingAbroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeingAbroadApplication.class, args);
+		System.out.println("\n Submit before 12-01-2023");
+	}
+
+}
