d50e4831b7d8c5422cde50cf04a889b092ce0a97
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-05 22:28:16 +0530

    entity class added

--- src/main/java/com/beingabroad/test/entity/Institution.java
+++ src/main/java/com/beingabroad/test/entity/Institution.java
@@ -1,4 +1,9 @@
 package com.beingabroad.test.entity;
 
 public class Institution {
+    private int stId;
+    private String name;
+    private String location;
+    private String contactNo;
+    private String address;
 }
