048e94e044099c29175e34def5ef43fe63355138
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-06 19:51:17 +0530

    @Getmapping()

--- src/main/java/com/beingabroad/test/controller/InstituteController.java
+++ src/main/java/com/beingabroad/test/controller/InstituteController.java
@@ -2,17 +2,18 @@ package com.beingabroad.test.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/being")
 public class InstituteController {
 
     // testEndPoint,
-    @GetMapping
+    @GetMapping("/test/{st}")
     @ResponseStatus(code = HttpStatus.CONTINUE)
-    public String testEndPoint()
-    {
-        return "Welcome!";
+    public String testEndPoint(String st) {
+        return "Welcome! "+st;
     }
 }
