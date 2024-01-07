ee32a5495b97ff8faa5c9240c9a5a751ecde677a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-08 00:27:58 +0530

    Endpoint to find the institute added

--- src/main/java/com/beingabroad/test/controller/InstituteController.java
+++ src/main/java/com/beingabroad/test/controller/InstituteController.java
@@ -1,5 +1,9 @@
 package com.beingabroad.test.controller;
 
+import com.beingabroad.test.dto.InstitutionDTO;
+import com.beingabroad.test.entity.Institution;
+import com.beingabroad.test.service.InstitutionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,10 +11,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/being")
 public class InstituteController {
 
+    @Autowired
+    private InstitutionService iService;
     // testEndPoint,
     @GetMapping("/test/{st}")
     @ResponseStatus(code = HttpStatus.CONTINUE)
     public String testEndPoint(@PathVariable String st) {
         return "Welcome! "+st;
     }
+
+    // INSERT INTO TABLE VALUES();
+    @PostMapping("/saveInstitute")
+    public Institution saveInstitute(@RequestBody InstitutionDTO institutionDTO){
+        return iService.saveInstitutte(institutionDTO);
+    }
+
+    // SELECT * FROM TABLE WHERE id=?,
+    @GetMapping("/getInstitute/{id}")
+    public Institution getInstitute(@PathVariable Integer id){
+        return iService.getInstitute(id);
+    }
+
 }
