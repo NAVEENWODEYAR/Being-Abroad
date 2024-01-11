94e517b0796c5f3c115977b1f11df6a6f4e20a3a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-11 21:39:15 +0530

    task comleted with less fulfillments

--- src/main/java/com/beingabroad/test/config/SecurityCinfig.java
+++ src/main/java/com/beingabroad/test/config/SecurityCinfig.java
@@ -1,4 +1,23 @@
 package com.beingabroad.test.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 public class SecurityCinfig {
+//
+//    @Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
+//    {
+//        http
+//                .csrf().disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/being/abroad").permitAll()
+//                .requestMatchers("/being/abroad/").authenticated()
+//
+//                .and().formLogin()
+//                .and().httpBasic();
+//
+//        return http.build();
+//    }
 }
